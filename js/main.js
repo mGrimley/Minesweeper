83e0dd63961afd6370ec32ab4703258e97c5dcc8
mGrimley <mich@elgrimley.com>
2022-09-21 14:27:24 -0700

    Cleaned up functions

--- js/main.js
+++ js/main.js
@@ -3,10 +3,9 @@ const MAX_WIDTH = 40
 const MAX_HEIGHT = 40
 const MAX_MINES = 99
 const DIFFICULTIES = {
-    //difficulty: [rows, cols, mines]
-    EASY: [9, 9, 10],
-    MEDIUM: [16, 16, 40],
-    HARD: [24, 24, 99],
+    EASY: {COLS: 9, ROWS: 9, MINES: 10},
+    MEDIUM: {COLS: 16, ROWS: 16, MINES: 40},
+    HARD: {COLS: 24, ROWS: 24, MINES: 10},
     CUSTOM: 'custom'
 }
 const STATES = {
@@ -17,17 +16,27 @@ const STATES = {
     UNCOVERED: 'uncovered',
 }
 
+const minefield = {    
+    rowNum: DIFFICULTIES.EASY.ROWS, 
+    colNum: DIFFICULTIES.EASY.COLS,
+    cells: [],
+}
+
+
 /*----- app's state (variables) -----*/ 
-let numMines = currentMines = DIFFICULTIES.EASY[2]
+let numMines = currentMines = DIFFICULTIES.EASY.MINES
+
 
 /*----- cached element references -----*/ 
 const minefieldEl = document.querySelector('.minefield')
 const mineCountEl = document.querySelector('.mine-count')
 
+
 // Buttons
 const quickResetBtnEl = document.querySelector('.quick-reset')
 const customResetBtnEl = document.querySelector('.custom-reset')
 const rulesBtnEl = document.querySelector('.rules')
+
 
 /*----- event listeners -----*/ 
 // Minefield
@@ -41,29 +50,23 @@ rulesBtnEl.addEventListener('click', handleRulesClick)
 
 
 /*----- functions -----*/
-
-
-
-
 function handleBoardLeftClick(evt) {
     quickResetBtnEl.disabled = false
 
     const targetCell = evt.target
-
-
+    
     if(targetCell.classList.contains('cell')) {
     const tmpCellState = targetCell.classList[1]
-
-        if(tmpCellState === 'flagged') {
-
-        } else if(tmpCellState === 'hidden') {
-            targetCell.classList.replace(tmpCellState, 'uncovered')
+        if(tmpCellState === STATES.HIDDEN) {
+            targetCell.classList.replace(tmpCellState, STATES.UNCOVERED)
+            // update corresponding cell's state to uncovered
         }
     }
 }
 
 function handleBoardRightClick(evt) {
     evt.preventDefault()
+
     quickResetBtnEl.disabled = false
 
     const targetCell = evt.target
@@ -71,48 +74,42 @@ function handleBoardRightClick(evt) {
     if(targetCell.classList.contains('cell')) {
         const tmpCellState = targetCell.classList[1]
 
-        if(tmpCellState === 'hidden') {
-            targetCell.classList.replace(tmpCellState, 'flagged')
+        if(tmpCellState === STATES.HIDDEN) {
+            targetCell.classList.replace(tmpCellState, STATES.FLAGGED)
             currentMines--
-            //currentMines-- ?
-        } else if(tmpCellState === 'flagged') {
-            targetCell.classList.replace(tmpCellState, 'hidden')
+        } else if(tmpCellState === STATES.FLAGGED) {
+            targetCell.classList.replace(tmpCellState, STATES.HIDDEN)
             currentMines++
         }
-
         
-        if(currentMines === 0) {
-            mineCountEl.style.color = 'green'
-            // set number green
-        } else if(currentMines < 0) {
-            mineCountEl.style.color = 'red'
-            // set number red
-        } else {
-            mineCountEl.style.color = 'black'
-            // set number black
-        }
+        renderMineCountEl()
 
         mineCountEl.textContent = currentMines
-        
+    }
+}
+function renderMineCountEl() {
+    if(currentMines === 0) {
+        mineCountEl.style.color = 'green'
+    } else if(currentMines < 0) {
+        mineCountEl.style.color = 'red'
+    } else {
+        mineCountEl.style.color = 'black'
     }
 }
 
 function handleResetClick(evt) {
     renderMinefield(minefield.rowNum, minefield.colNum)
 }
-
 function handleCustomResetClick(evt) {
     console.log(evt.target)
 }
-
 function handleRulesClick(evt) {
     console.log(evt.target)
 }
-
 function renderMinefield() {
     // Reset quick reset button
     quickResetBtnEl.disabled = true
-
+    
     // Reset mineCountEl color
     mineCountEl.style.color = 'black'
 
@@ -124,35 +121,14 @@ function renderMinefield() {
     while(minefieldEl.firstChild) {
         minefieldEl.removeChild(minefieldEl.lastChild)
     }
-    minefield.cells = []
-    
-    // Create specified number of cells 
-    // let tmpIdx = 0;
-    // for(let r = 1; r <= minefield.rowNum; r++) {
-    //     const row = []
-    //     for(let c = 1; c <= minefield.colNum; c++) {
-    //         const cellDiv = document.createElement('div')
-    //         cellDiv.classList.add('cell', 'hidden', `r${r}`, `c${c}`, `i${++tmpIdx}`)
-    //         // document.querySelector('.minefield').appendChild(cellDiv)
-
-    //         const cell = {
-    //             cellDiv,
-    //             r: r,
-    //             c: c,
-    //             mine: false,
-    //         }
-
-    //         row.push(cell)
-    //     }
-    //     minefield.cells.push(row)
-    // }
+     minefield.cells = []
 
     let tmpIdx = 0;
     for(let c = 1; c <= minefield.colNum; c++) {
         const col = []
         for(let r = 1; r <= minefield.rowNum; r++) {
             const cellDiv = document.createElement('div')
-            cellDiv.classList.add('cell', 'hidden', `c${c}`, `r${r}`, `i${++tmpIdx}`)
+            cellDiv.classList.add('cell', STATES.HIDDEN, `c${c}`, `r${r}`, `i${++tmpIdx}`)
             // document.querySelector('.minefield').appendChild(cellDiv)
 
             const cell = {
@@ -163,8 +139,8 @@ function renderMinefield() {
             }
 
             col.push(cell)
-        }
-        minefield.cells.push(col)
+         }
+         minefield.cells.push(col)
     }
 
     drawCells()
@@ -176,15 +152,8 @@ function drawCells() {
             minefieldEl.appendChild(cell.cellDiv)
         })
     })
-
-    // minefieldEl.style.setProperty('--row-num', DIFFICULTIES.EASY[0])
-    // minefieldEl.style.setProperty('--col-num', DIFFICULTIES.EASY[1])
-}
-
-const minefield = {    
-    rowNum: parseInt(getComputedStyle(minefieldEl).getPropertyValue('--row-num')), 
-    colNum: parseInt(getComputedStyle(minefieldEl).getPropertyValue('--col-num')),
-    cells: [],
+    // minefieldEl.style.setProperty('--row-num', DIFFICULTIES.EASY.ROWS)
+    // minefieldEl.style.setProperty('--col-num', DIFFICULTIES.EASY.COLS)
 }
 
 function init() {
@@ -193,10 +162,9 @@ function init() {
 
 init()
 
+
 // const card = {
 //     suit: 'c',
 //     value: 'A'
-    
 // }
-
 // document.querySelector('div').classList.add(card.suit + card.value)
